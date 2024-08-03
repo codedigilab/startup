08104aaef60ea9ab1ddc39cc994cd6eb41f0ec4b
CodeDigiLab <159695671+CodeDigiLab@users.noreply.github.com>
2024-08-04 00:26:36 +0530

    dfg

--- include/data.js
+++ include/data.js
@@ -59,4 +59,174 @@ const whatwedo  = [
     }
 ]
 
+const ourservices  = [
+    {     
+        title:'Web Development',
+        image:'assets/img/service/service_block_1.webp',
+        link:'#',
+    },
+    {     
+        title:'Digital Marketing',
+        image:'assets/img/service/service_block_2.webp',
+        link:'#',
+    },
+    {     
+        title:'Apps Development',
+        image:'assets/img/service/service_block_3.webp',
+        link:'#',
+        
+    },
+    {     
+        title:'E-Commerce',
+        image:'assets/img/service/service_block_4.webp',
+        link:'#',
+    },
+    
+    {     
+        title:'Web Development',
+        image:'assets/img/service/service_block_1.webp',
+        link:'#',
+    },
+    {     
+        title:'Digital Marketing',
+        image:'assets/img/service/service_block_2.webp',
+        link:'#',
+    },
+    {     
+        title:'Apps Development',
+        image:'assets/img/service/service_block_3.webp',
+        link:'#',
+        
+    },
+    {     
+        title:'E-Commerce',
+        image:'assets/img/service/service_block_4.webp',
+        link:'#',
+    },
+]
 
+const workingprocess  = [
+    {
+        number: '01',
+        title: 'Meet Our Team',
+        details:'Proactively envisioned multimedia based expertise cross-media growth',
+        link:'#',
+    },
+    {
+        number: '02',
+        title: 'Get Estimation',
+        details:'Proactively envisioned multimedia based expertise cross-media growth',
+        link:'#',
+    },
+    {
+        number: '03',
+        title: 'Start Project',
+        details:'Proactively envisioned multimedia based expertise cross-media growth',
+        link:'#',
+        
+    },
+    {
+        number: '04',
+        title: 'Deliver Project',
+        details:'Proactively envisioned multimedia based expertise cross-media growth',
+        link:'#',
+    },
+    
+]
+
+const expertteam  = [
+    {
+        image:'https://img.freepik.com/premium-photo/indian-students-isolated-white-background_988871-5.jpg',
+        title: 'Priya',
+        whatsapp:'+917361040504',
+        link:'#',
+    },
+    {
+        image:'https://img.freepik.com/premium-photo/indian-students-isolated-white-background_988871-5.jpg',
+        title: 'Priya',
+        whatsapp:'+917361040504',
+        link:'#',
+    },
+    {
+        image:'https://img.freepik.com/premium-photo/indian-students-isolated-white-background_988871-5.jpg',
+        title: 'Priya',
+        whatsapp:'+917361040504',
+        link:'#',
+        
+    },
+    {
+        image:'https://img.freepik.com/premium-photo/indian-students-isolated-white-background_988871-5.jpg',
+        title: 'Priya',
+        whatsapp:'+917361040504',
+        link:'#',
+    },
+    {
+        image:'https://img.freepik.com/premium-photo/indian-students-isolated-white-background_988871-5.jpg',
+        title: 'Priya',
+        whatsapp:'+917361040504',
+        link:'#',
+        
+    },
+    {
+        image:'https://img.freepik.com/premium-photo/indian-students-isolated-white-background_988871-5.jpg',
+        title: 'Priya',
+        whatsapp:'+917361040504',
+        link:'#',
+    },
+    
+]
+
+const testimonial  = [
+    {
+        image1:'assets/img/testimonial/testi_3_1.jpg',
+        image2:'assets/img/brand/testi_logo_1.svg',
+        title: 'Amit Kumar',
+        details:'We tried another company that one of our partners had used but they didn’t work out. I feel that Code Digi Lab does a better investigation of what we’re asking for.',
+        link:'#',
+    },
+    {
+        image1:'assets/img/testimonial/testi_3_1.jpg',
+        image2:'assets/img/brand/testi_logo_1.svg',
+        title: 'Praveen Kumar Mishra',
+        details:'I was impressed by Code Digi Lab prices, especially for the project I wanted to do and in comparison to the quotes I received from a lot of other companies.',
+        link:'#',
+    },
+    {
+        image1:'assets/img/testimonial/testi_3_1.jpg',
+        image2:'assets/img/brand/testi_logo_1.svg',
+        title: 'Reyaz Alam',
+        details:'Code Digi Lab developed a new version of the MyMediAds service. When we turned to the CodeDigiLab experts we had already been using an MVP which we would like to improve.',
+        link:'#',
+        
+    },
+    {
+        image1:'assets/img/testimonial/testi_3_1.jpg',
+        image2:'assets/img/brand/testi_logo_1.svg',
+        title: 'Mohan Dash',
+        details:'When looking for a strategic IT-partner for the development of a corporate ERP solution, we chose Code Digi Lab. The company proved itself a reliable provider of IT services.',
+        link:'#',
+        
+    }
+   
+    
+]
+
+const blog  = [
+    {
+        image:'assets/img/service/service_block_2.webp',
+        title: 'Meet the Leaders: Best Digital Marketing',
+        link:'#',
+    },
+    {
+        image:'assets/img/service/service_block_1.webp',
+        title: ' Website Development Companies',
+        link:'#',
+    },
+    {
+        image:'assets/img/service/service_block_3.webp',
+        title: 'Mobile App Development Firms',
+        link:'#',
+        
+    }
+    
+]
